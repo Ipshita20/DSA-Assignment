2113132ea0de6b82a2f5f8c13c8f69ca216771e1
Ipshita20 <singlaipshita20@gmail.com>
2025-09-02 00:19:56 +0530

    q2.cpp

--- Assignment-3/q2.cpp
+++ Assignment-3/q2.cpp
@@ -1,18 +1,14 @@
 #include <iostream>
 #include <string>
 using namespace std;
-
 #define MAX 100   
-
 class Stack {
     char arr[MAX];
     int top;
-
 public:
     Stack() {
         top = -1;
     }
-
     void push(char c) {
         if (top == MAX - 1) {
             cout << "Stack Overflow!" << endl;
@@ -20,7 +16,6 @@ public:
             arr[++top] = c;
         }
     }
-
     char pop() {
         if (top == -1) {
             cout << "Stack Underflow!" << endl;
@@ -29,27 +24,25 @@ public:
             return arr[top--];
         }
     }
-
-    bool isEmpty() {
-        return (top == -1);
+    int isEmpty() {   // return int instead of bool
+        if (top == -1)
+            return 1;
+        else
+            return 0; 
     }
 };
-
 int main() {
     string str;
     cout << "Enter a string: ";
     cin >> str;   
-
     Stack s;
     for (int i = 0; i < str.length(); i++) {
         s.push(str[i]);
     }
     string rev = "";
-    while (!s.isEmpty()) {
+    while (s.isEmpty() == 0) {  
         rev += s.pop();
     }
-
     cout << "Reversed string: " << rev << endl;
-
     return 0;
 }
