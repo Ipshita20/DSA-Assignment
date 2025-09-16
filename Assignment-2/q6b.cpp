33b3654b43121c33ef4a974be0d97974c73864b3
Ipshita20 <singlaipshita20@gmail.com>
2025-09-16 12:29:51 +0530

    Update q6b.cpp

--- Assignment-2/q6b.cpp
+++ Assignment-2/q6b.cpp
@@ -14,44 +14,59 @@ int addSparse(int A[][3], int sizeA, int B[][3], int sizeB, int C[][3]) {
             C[k][0] = A[i][0];
             C[k][1] = A[i][1];
             C[k][2] = A[i][2] + B[j][2];
-            i++; j++; k++;
+            i++; 
+            j++; 
+            k++;
         }
         else if (A[i][0] < B[j][0] || (A[i][0] == B[j][0] && A[i][1] < B[j][1])) {
-            C[k][0] = A[i][0]; C[k][1] = A[i][1]; C[k][2] = A[i][2];
-            i++; k++;
+            C[k][0] = A[i][0]; 
+            C[k][1] = A[i][1]; 
+            C[k][2] = A[i][2];
+            i++;
+            k++;
         }
         else {
-            C[k][0] = B[j][0]; C[k][1] = B[j][1]; C[k][2] = B[j][2];
-            j++; k++;
+            C[k][0] = B[j][0];
+            C[k][1] = B[j][1];
+            C[k][2] = B[j][2];
+            j++; 
+            k++;
         }
     }
-    while (i < sizeA) { C[k][0] = A[i][0]; C[k][1] = A[i][1]; C[k][2] = A[i][2]; i++; k++; }
-    while (j < sizeB) { C[k][0] = B[j][0]; C[k][1] = B[j][1]; C[k][2] = B[j][2]; j++; k++; }
+    while (i < sizeA) 
+    { 
+        C[k][0] = A[i][0]; 
+        C[k][1] = A[i][1];
+        C[k][2] = A[i][2]; 
+        i++;
+        k++; 
+    }
+    while (j < sizeB) {
+        C[k][0] = B[j][0]; 
+        C[k][1] = B[j][1];
+        C[k][2] = B[j][2];
+        j++; 
+        k++; 
+    }
     return k;
 }
-
 int main() {
     int m, n;
     cout << "Enter size of matrices (m x n): ";
     cin >> m >> n;
-
     int a[m][n], b[m][n];
     cout << "Enter elements of Matrix A:\n";
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
             cin >> a[i][j];
-
     cout << "Enter elements of Matrix B:\n";
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
             cin >> b[i][j];
-
-    // Convert A to sparse
     int sizeA = 0;
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
             if (a[i][j] != 0) sizeA++;
-
     int A[sizeA][3], rowA = 0;
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
@@ -59,13 +74,10 @@ int main() {
                 A[rowA][0] = i; A[rowA][1] = j; A[rowA][2] = a[i][j];
                 rowA++;
             }
-
-    // Convert B to sparse
     int sizeB=0;
     for (int i=0; i<m; i++)
         for (int j=0; j<n; j++)
             if (b[i][j]!=0) sizeB++;
-
     int B[sizeB][3], rowB=0;
     for (int i=0; i<m; i++)
         for (int j=0; j<n; j++)
@@ -85,3 +97,4 @@ int main() {
     dispSparse(C, sizeC);
     return 0;
 }
+
