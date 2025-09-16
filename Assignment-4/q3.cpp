a63468a8875e8822d4c1f988974feef09e5aaa7b
Ipshita20 <singlaipshita20@gmail.com>
2025-09-16 11:10:01 +0530

    q3.cpp

--- Assignment-4/q3.cpp
+++ Assignment-4/q3.cpp
@@ -8,20 +8,20 @@ void interleave(int arr[], int n) {
     int half=n/2;
     int firstHalf[half], secondHalf[half];
     for (int i=0; i<half; i++)
-        firstHalf[i] = arr[i];
+        firstHalf[i]=arr[i];
     for (int i=0; i<half; i++)
-        secondHalf[i] = arr[i+half];
+        secondHalf[i]=arr[i+half];
     int k=0;
     for (int i=0; i<half; i++) {
-        arr[k++] = firstHalf[i];
-        arr[k++] = secondHalf[i];
+        arr[k++]=firstHalf[i];
+        arr[k++]=secondHalf[i];
     }
 }
 int main() {
-    int arr[] = {4, 7, 11, 20, 5, 9};
-    int n = sizeof(arr)/sizeof(arr[0]);
+    int arr[]={4, 7, 11, 20, 5, 9};
+    int n=sizeof(arr)/sizeof(arr[0]);
     interleave(arr, n);
-    for (int i = 0; i < n; i++)
+    for (int i=0; i<n; i++)
         cout<<arr[i]<<" ";
     cout<<endl;  
     return 0;
