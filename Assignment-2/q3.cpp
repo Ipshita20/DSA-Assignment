f7c32192d195898f47d5daada46d4d36e98ce7f8
Ipshita20 <singlaipshita20@gmail.com>
2025-09-16 10:13:11 +0530

    q3.cpp

--- Assignment-2/q3.cpp
+++ Assignment-2/q3.cpp
@@ -1,24 +1,22 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int findMissingBinary(int arr[], int n) {
     int left = 0, right = n - 1;
+    int start = arr[0];  
     while (left <= right) {
         int mid = (left + right) / 2;
-        if (arr[mid] != mid + 1) {
-            if (mid == 0 || arr[mid - 1] == mid) 
-            return mid + 1;
+        if (arr[mid] != start + mid) {
+            if (mid == 0 || arr[mid - 1] == start + mid - 1) 
+                return start + mid;
             right = mid - 1;
-        } 
-        else {
+        } else {
             left = mid + 1;
         }
     }
     return -1; 
 }
-
 int main() {
-    int arr[] = {1, 2, 3, 5, 6}; 
+    int arr[] = {5, 6, 7, 9};
     int n = sizeof(arr) / sizeof(arr[0]);
     cout << "Missing number is " << findMissingBinary(arr, n);
     return 0;
