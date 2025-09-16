6929861822450b7ae4f9db07a3932c16d8ca071a
Ipshita20 <singlaipshita20@gmail.com>
2025-09-16 12:28:07 +0530

    q8.cpp

--- Assignment-2/q8.cpp
+++ Assignment-2/q8.cpp
@@ -1,25 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main() {
     int n;
-    cout << "Enter size of array: ";
-    cin >> n;
+    cout<<"Enter size of array: ";
+    cin>>n;
     int arr[n];
-    cout << "Enter " << n << " elements:\n";
+    cout<<"Enter "<<n<<" elements:\n";
     for(int i=0; i<n; i++)
-        cin >> arr[i];
-    int count = 0;
+        cin>>arr[i];
+    int count=0;
     for(int i=0; i<n; i++) {
-        int flag = 0;   
+        int flag=0;   
         for(int j=0; j<i; j++) {
-            if(arr[i] == arr[j]) {
-                flag = 1;   
+            if(arr[i]==arr[j]) {
+                flag=1;   
                 break;
             }
         }
-        if(flag == 0)   
+        if(flag==0)   
             count++;
     }
-    cout << "Total distinct elements = " << count << endl;
+    cout<<"Total distinct elements = "<<count<<endl;
     return 0;
 }
+
