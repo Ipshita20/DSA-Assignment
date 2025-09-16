00c9ef3eca6fcb3b85bab8dd619eddee4e0c15ce
Ipshita20 <singlaipshita20@gmail.com>
2025-09-16 10:40:20 +0530

    q2.cpp

--- Assignment-3/q2.cpp
+++ Assignment-3/q2.cpp
@@ -1,48 +1,51 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<string>
 using namespace std;
-#define MAX 100   
-class Stack {
+#define MAX 100
+class Stack{
     char arr[MAX];
     int top;
 public:
-    Stack() {
-        top = -1;
-    }
-    void push(char c) {
-        if (top == MAX - 1) {
-            cout << "Stack Overflow!" << endl;
-        } else {
-            arr[++top] = c;
+    Stack()
+{
+    top=-1;
+}
+    void push(char c){
+        if(top==MAX-1){
+            cout<<"Stack Overflow!"<<endl;
+        }
+        else{
+            arr[++top]=c;
         }
     }
-    char pop() {
-        if (top == -1) {
-            cout << "Stack Underflow!" << endl;
-            return '\0';   
-        } else {
+    char pop(){
+        if(top==-1){
+            cout<<"Stack Underflow!"<<endl;
+            return '\0';
+        }
+        else{
             return arr[top--];
         }
     }
-    int isEmpty() {   // return int instead of bool
-        if (top == -1)
-            return 1;
-        else
-            return 0; 
-    }
+    bool isEmpty()
+{
+    return top==-1;
+}
 };
-int main() {
+int main(){
     string str;
-    cout << "Enter a string: ";
-    cin >> str;   
+    cout<<"Enter a string: ";
+    cin>>str;
     Stack s;
-    for (int i = 0; i < str.length(); i++) {
+    for(int i=0;i<str.length();i++)
+    {
         s.push(str[i]);
     }
-    string rev = "";
-    while (s.isEmpty() == 0) {  
-        rev += s.pop();
+    string rev="";
+    while(!s.isEmpty())
+    {
+        rev+=s.pop();
     }
-    cout << "Reversed string: " << rev << endl;
+    cout<<"Reversed string: "<<rev<<endl;
     return 0;
 }
