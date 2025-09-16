a256a4e502eae8ab04331bc1d06b4350eb07f102
Ipshita20 <singlaipshita20@gmail.com>
2025-09-16 12:27:01 +0530

    q7.cpp

--- Assignment-2/q7.cpp
+++ Assignment-2/q7.cpp
@@ -1,11 +1,10 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int Inversions(int arr[], int n) {
-    int count = 0;
-
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = i + 1; j < n; j++) {
-            if (arr[i] > arr[j]) {
+    int count=0;
+    for (int i=0; i<n-1; i++) {
+        for (int j=i+1; j<n; j++) {
+            if (arr[i]>arr[j]) {
                 count++;
             }
         }
@@ -14,14 +13,15 @@ int Inversions(int arr[], int n) {
 }
 int main() {
     int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
+    cout<<"Enter number of elements: ";
+    cin>>n;
     int arr[n];
-    cout << "Enter elements: ";
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    cout<<"Enter elements: ";
+    for (int i=0; i<n; i++) {
+        cin>>arr[i];
     }
-    int result = Inversions(arr, n);
-    cout << "Total Inversions = " << result << endl;
+    int result=Inversions(arr, n);
+    cout<<"Total Inversions = "<<result<<endl;
     return 0;
 }
+
