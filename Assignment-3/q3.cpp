ff185d28fb34fe6123d1e16f5be66d13dd188cad
Ipshita20 <singlaipshita20@gmail.com>
2025-09-16 10:47:17 +0530

    q3.cpp

--- Assignment-3/q3.cpp
+++ Assignment-3/q3.cpp
@@ -27,31 +27,39 @@ public:
         }
     }
     char peek() {
-        if (top == -1) return '\0';
+        if (top == -1) 
+        return '\0';
         return arr[top];
     }
     int isEmpty() {
-        if (top == -1) return 1;  
-        else return 0;            
+        if (top == -1) 
+        return 1;  
+        else 
+        return 0;            
     }
 };
-int isMatchingPair(char open, char close) {
-    if (open == '(' && close == ')') return 1;
-    if (open == '{' && close == '}') return 1;
-    if (open == '[' && close == ']') return 1;
+int Match(char open, char close) {
+    if (open == '(' && close == ')') 
+    return 1;                                                                                                                                  
+    if (open == '{' && close == '}') 
+    return 1;
+    if (open == '[' && close == ']') 
+    return 1;
     return 0;
 }
-int isBalanced(string expr) {
+int Balanced(string exp) {
     Stack s;
-    for (int i = 0; i < expr.length(); i++) {
-        char ch = expr[i];
+    for (int i = 0; i < exp.length(); i++) {
+        char ch = exp[i];
         if (ch == '(' || ch == '{' || ch == '[') {
             s.push(ch);
         }
         else if (ch == ')' || ch == '}' || ch == ']') {
-            if (s.isEmpty() == 1) return 0;  
+            if (s.isEmpty() == 1)
+             return 0;  
             char topChar = s.pop();
-            if (isMatchingPair(topChar, ch) == 0) return 0;
+            if (Match(topChar, ch) == 0) 
+            return 0;
         }
     }
     if (s.isEmpty() == 1) 
@@ -60,11 +68,11 @@ int isBalanced(string expr) {
     return 0;                    
 }
 int main() {
-    string expr;
-    cout << "Enter an expression: ";
-    cin >> expr;
-    if (isBalanced(expr) == 1)
-        cout << "Balanced Expression" << endl;
+    string exp;
+    cout << "Enter an expession: ";
+    cin >> exp;
+    if (Balanced(exp) == 1)
+        cout << "Balanced expession" << endl;
     else
         cout << "Not Balanced" << endl;
     return 0;
