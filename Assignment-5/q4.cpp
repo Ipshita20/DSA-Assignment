31c38ef97b78163d93390a086600639da0fb5810
Ipshita20 <singlaipshita20@gmail.com>
2025-09-16 11:33:17 +0530

    Update q4.cpp

--- Assignment-5/q4.cpp
+++ Assignment-5/q4.cpp
@@ -1,60 +1,45 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-// Node structure
-struct Node {
+struct Node{
     int data;
     Node* next;
 };
-
-// Function to create a new node
-Node* newNode(int data) {
-    Node* temp = new Node();
-    temp->data = data;
-    temp->next = NULL;
+Node* newNode(int data){
+    Node* temp=new Node();
+    temp->data=data;
+    temp->next=NULL;
     return temp;
 }
-
-// Function to reverse a linked list
-Node* reverseList(Node* head) {
-    Node* prev = NULL;
-    Node* curr = head;
-    Node* next = NULL;
-
-    while (curr != NULL) {
-        next = curr->next;   // store next node
-        curr->next = prev;   // reverse the link
-        prev = curr;         // move prev forward
-        curr = next;         // move curr forward
+Node* reverseList(Node* head){
+    Node* prev=NULL;
+    Node* curr=head;
+    Node* next=NULL;
+    while(curr!=NULL){
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
     }
-    return prev; // new head
+    return prev;
 }
-
-// Function to print linked list
-void printList(Node* head) {
-    while (head != NULL) {
-        cout << head->data;
-        if (head->next != NULL) cout << "->";
-        head = head->next;
+void printList(Node* head){
+    while(head!=NULL){
+        cout<<head->data;
+        if(head->next!=NULL)
+            cout<<"->";
+        head=head->next;
     }
-    cout << "->NULL\n";
+    cout<<"->NULL"<<endl;
 }
-
-int main() {
-    // Creating linked list 1->2->3->4->NULL
-    Node* head = newNode(1);
-    head->next = newNode(2);
-    head->next->next = newNode(3);
-    head->next->next->next = newNode(4);
-
-    cout << "Original List: ";
+int main(){
+    Node* head=newNode(1);
+    head->next=newNode(2);
+    head->next->next=newNode(3);
+    head->next->next->next=newNode(4);
+    cout<<"Original List: ";
     printList(head);
-
-    // Reverse the list
-    head = reverseList(head);
-
-    cout << "Reversed List: ";
+    head=reverseList(head);
+    cout<<"Reversed List: ";
     printList(head);
-
     return 0;
 }
